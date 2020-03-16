746859565d78bea096c8ec6929b6439499e810a6
Junaid Shad <xadxunaid@gmail.com>
2020-03-16 18:44:38 +0500

    Modification

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,12 +1,12 @@
 <template>
-    <v-layout row wrap align-center justify-center>
-        <v-dialog v-model="notifications" width="300">
-            <v-card>
-                <v-card-title>
-                    Site Under Construction
-                </v-card-title>
-            </v-card>
-        </v-dialog>
+    <v-layout row wrap align-center justify-center my-5 py-5>
+        <v-flex xs12 text-center mt-5>
+            <img src="../../../public/images/under_construction_2.gif" height="400" width="600"/>
+        </v-flex>
+        <v-flex xs12>
+            <p :align="'center'" style="font-size: 24px">Site Under Construction</p>
+        </v-flex>
+
     </v-layout>
 </template>
 
